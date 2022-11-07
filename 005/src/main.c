9fe6c733530a08e5c5c7cdc45170eeced5f5cebd
Dmitry Mikhaylov <jerastd@gmail.com>
2022-11-07 20:02:29 +0300

    с циклом for

--- 005/src/main.c
+++ 005/src/main.c
@@ -10,9 +10,12 @@ main()
  step = 20; /* шаг */
  fahr = lower;
 printf("По Фаренгейту\tПо Цельсию\n");
- while (fahr <= upper) {
+ /*while (fahr <= upper) {
  celsius = (5.0/9.0) * (fahr-32.0);
  printf ("%6.0f\t\t%6.1f\n", fahr, celsius);
  fahr = fahr + step;
- }
+}*/
+for(fahr = 0; fahr <= 300; fahr = fahr + 20){
+  printf("%6.0f\t\t%3.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+}
 }
