d97c1eaf0a1c616cc83265daecbdd93c3e81290f
Dmitry Mikhaylov <jerastd@gmail.com>
2022-11-07 20:35:32 +0300

    с #define

--- 005/src/main.c
+++ 005/src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /* печать таблицы температур по Фаренгейту и Цельсию для
 fahr = 0, 20 ... 300; вариант с плавающей точкой */
+#define LOWER 0 /* нижняя граница таблицы */
+#define UPPER 300 /* верхняя граница */
+#define STEP 20 /* размер шага */
 main()
 {
  int fahr;
@@ -15,7 +18,7 @@ printf("По Фаренгейту\tПо Цельсию\n");
  printf ("%6.0f\t\t%6.1f\n", fahr, celsius);
  fahr = fahr + step;
 }*/
-for(fahr = 300; fahr >= 0; fahr = fahr - 20){
+for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP){
   printf("%6d\t\t%3.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
 }
 }
