7d978419c12b3bb679cd4f020f76c7f97f6f3f0b
Dmitry Mikhaylov <jerastd@gmail.com>
2022-11-07 20:13:39 +0300

    изменение

--- 005/src/main.c
+++ 005/src/main.c
@@ -3,12 +3,12 @@
 fahr = 0, 20 ... 300; вариант с плавающей точкой */
 main()
 {
- float fahr, celsius;
- int lower, upper, step;
- lower = 0; /* нижняя граница таблицы температур */
- upper = 300; /* верхняя граница */
- step = 20; /* шаг */
- fahr = lower;
+ int fahr;
+ // int lower, upper, step;
+ //lower = 0; /* нижняя граница таблицы температур */
+ // upper = 300; /* верхняя граница */
+// step = 20; /* шаг */
+ //fahr = lower;*/
 printf("По Фаренгейту\tПо Цельсию\n");
  /*while (fahr <= upper) {
  celsius = (5.0/9.0) * (fahr-32.0);
@@ -16,6 +16,6 @@ printf("По Фаренгейту\tПо Цельсию\n");
  fahr = fahr + step;
 }*/
 for(fahr = 0; fahr <= 300; fahr = fahr + 20){
-  printf("%6.0f\t\t%3.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+  printf("%6d\t\t%3.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
 }
 }
